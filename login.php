d851f70840c92f40740f63073e71470193112082
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-03-21 23:57:02 +0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,21 @@
+<!-- <?php
+session_start();
+if(isset($_SESSION['username'])) {
+	header('Location: dashboard.php');
+	exit();
+}
+
+if(isset($_POST['login'])) {
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	// TODO: validate username and password
+
+	// TODO: check username and password in database
+
+	// TODO: set session variables and redirect to dashboard
+}
+?> -->
+<?php
+echo"hi";
+?>
